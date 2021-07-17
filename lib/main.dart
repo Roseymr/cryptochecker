327b21750f7413b583b7606087478643bfc29649
AndreClerigo Desktop <andreclerigo@oultook.com>
2021-07-17 21:31:13 +0100

    currency change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/widgets.dart';
 import 'binance/binance.dart';
 
@@ -13,18 +14,36 @@ var customColors = {
 void main() => runApp(MyApp());
 
 var rest = Binance();
+String? selectedCurrency = 'EUR';
+Map<String, IconData> currencyIcon = {
+  'EUR': Icons.euro,
+  'USDT': Icons.attach_money,
+};
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return RestartWidget(
+        child: MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: BalanceWidget(),
-        ),
+        body: new Container(
+            child: new Column(
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(top: 25.0, right: 25.0),
+              alignment: Alignment.topRight,
+              child: LanguageWidget(),
+            ),
+            new Container(
+              height: 600,
+              alignment: Alignment.center,
+              child: BalanceWidget(),
+            ),
+          ],
+        )),
         backgroundColor: customColors['background'],
       ),
-    );
+    ));
   }
 }
 
@@ -33,18 +52,147 @@ class BalanceWidget extends StatefulWidget {
   _BalanceState createState() => _BalanceState();
 }
 
+class LanguageWidget extends StatefulWidget {
+  @override
+  _LanguageState createState() => _LanguageState();
+}
+
+class RestartWidget extends StatefulWidget {
+  RestartWidget({required this.child});
+
+  final Widget child;
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_RestartWidgetState>()!.restartApp();
+  }
+
+  @override
+  _RestartWidgetState createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
+
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child,
+    );
+  }
+}
+
 Future<String> printData(AccountInfo? acc) async {
   String res = '';
+
+  /*Make a total money function*/
 
   if (acc != null)
     for (Balance b in acc.balances)
       if (b.free != 0) {
-        AveragedPrice avg = await rest.averagePrice(b.asset + 'EUR');
+        AveragedPrice avg =
+            await rest.averagePrice(b.asset + '$selectedCurrency');
+        TickerStats stat = await rest.dailyStats(b.asset + '$selectedCurrency');
         res +=
-            '${b.asset}: ${b.free} - ${(avg.price * b.free).toStringAsFixed(2)} EUR \n';
+            '${b.asset}: ${b.free} - ${(avg.price * b.free).toStringAsFixed(2)} $selectedCurrency - Last Day: ${stat.priceChangePercent}%\n';
       }
 
   return res;
+}
+
+class _LanguageState extends State<LanguageWidget> {
+  void _setCurrency() async {
+    String curr;
+
+    if (selectedCurrency == 'EUR')
+      curr = 'USDT';
+    else
+      curr = 'EUR';
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('currency', curr);
+
+    setState(() {
+      selectedCurrency = curr;
+    });
+  }
+
+  void _getCurrency() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? currencyName = prefs.getString('currency');
+
+    setState(() {
+      selectedCurrency = currencyName;
+    });
+  }
+
+  String _currencyOption() {
+    if (selectedCurrency == 'EUR') return 'USDT';
+    return 'EUR';
+  }
+
+  @override
+  void initState() {
+    _getCurrency();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        width: 200,
+        alignment: Alignment.topRight,
+        child: new Container(
+            decoration: BoxDecoration(
+              color: customColors['secondary'],
+              borderRadius: new BorderRadius.vertical(
+                  top: Radius.circular(15), bottom: Radius.circular(15)),
+            ),
+            child: ExpansionTile(
+              title: new Row(
+                children: <Widget>[
+                  Icon(
+                    currencyIcon[selectedCurrency],
+                    color: Colors.white,
+                  ),
+                  Text(
+                    'Currency ($selectedCurrency)',
+                    style: const TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ],
+              ),
+              children: <Widget>[
+                new Container(
+                    height: 45.0,
+                    child: GestureDetector(
+                      onTap: () {
+                        _setCurrency();
+                        RestartWidget.restartApp(context);
+                      },
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            currencyIcon[_currencyOption()],
+                            color: Colors.white,
+                          ),
+                          Text(
+                            '${_currencyOption()}',
+                            style: const TextStyle(
+                                fontSize: 15, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )),
+              ],
+            )));
+  }
 }
 
 class _BalanceState extends State<BalanceWidget> {
@@ -56,7 +204,6 @@ class _BalanceState extends State<BalanceWidget> {
       child: FutureBuilder<AccountInfo>(
         future: rest.accountInfo(DateTime.now().millisecondsSinceEpoch),
         builder: (BuildContext context, AsyncSnapshot<AccountInfo> snapshot) {
-          // AsyncSnapshot<Your object type>
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: Text('Please wait its loading...'));
           } else {
